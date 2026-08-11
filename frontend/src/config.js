d9fc7aa5d4da37b33690c46a1ddcccd94dc84b60
uukomadu <ugoukomadu@gmail.com>
2026-08-10 20:01:41 -0500

    Route frontend API requests to backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const API_URL =
-  'http://devops-challenge-alb-1270001619.us-east-2.elb.amazonaws.com';
+  'http://devops-challenge-alb-1270001619.us-east-2.elb.amazonaws.com/api';
 
 export default API_URL;
 
